38ca5360e57c44bbb3fc1c8f2b4a12fa3d591c0e
Hayeongjin <109400761+nooooooob12@users.noreply.github.com>
2022-10-20 10:12:32 +0900

    Add files via upload

---
+++ component01/One.js
@@ -0,0 +1,8 @@
+
+function One(){
+    return(
+        <h1>첫번째 컴포넌트</h1>
+    )
+}
+
+export default One;
---
+++ component01/Two.js
@@ -0,0 +1,8 @@
+
+function Two(){
+    return(
+        <h1>두번째 컴포넌트</h1>
+    )
+}
+
+export default Two;
